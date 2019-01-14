0eb5512076acfc77daba5a3bf88159eebcea9002
Bhushan Gaikwad <43483545+bhushangaykawad@users.noreply.github.com>
2019-01-14 17:47:00 +0530

    Create solution.php

---
+++ solution.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+*  Video About :    Solution of Coding Challenge Named "Target Sum By 2".
+*  Challenger  :    Hitesh Chaudhary via youtube challenge series.
+*  Name        :    Bhushan Gaykawad
+*  Link        :    https://www.youtube.com/watch?v=HJxQUDaNOgI
+*  Problem     :    In a given one-dimentional array which is having integers (positive, negative ie. not float or double) as elements,
+*                   find the pair of elements whos sum will be another given integer.
+*  Ex.         :    We have an array having [ 4, 5, 12, -2, 8, 9 ] and the given sum is 10,
+*                   then we have to find pair whos sum is 10. By observing these elements we can simply say [ 12, -2 ].
+*  Conditions  :    We want optimized code.
+*/
+
+class Solution
+{
+    public function twoSum($given, $target)
+    {
+        $checker = []; // space complexity O(n)
+        
+        //we are only accessing elements in array once, so time complexity O(n)
+        for ($i=0; $i < sizeof($given); $i++) {
+            $difference = $target - $given[$i]; // 10 - 4 = 6
+            if (key_exists($difference, $checker)) { // map functions so O(1)
+                echo 'Pair found: ' . $difference . ',' . $given[$i] . '<br>';
+            }
+            $checker[$given[$i]] = $i;
+            //     $checker[$i] = $given[$i];
+            //     echo '<pre>';
+            //     print_r($checker);
+            //     echo '</pre><br>';
+        }
+    }
+}
+
+
+$given = [ 4, 5, 12, -2, 6, 9 ];
+$sol = new Solution();
+$sol->twoSum($given, 10);
